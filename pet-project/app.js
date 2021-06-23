511e5a085201dbe4d7dabd0c07dd77e6243d1412
alinakoshnik <alinakoshnik@gmail.com>
2021-06-23 13:46:54 +0300

    commit 230621

--- pet-project/app.js
+++ pet-project/app.js
@@ -2,8 +2,14 @@ $(document).ready(() => {
     $(".slider-content").slick({
         dots: true,
         easing: 'ease',
+        // prevArrow: 'button-prev',
+        // nextArrow: 'button-next',
+
     });
 
+    // fetch('http://192.168.0.107:8080/data')
+    // .then(res => res.json())
+    // .then(data => console.log(data));
 })
 
 $(document).ready(function() {
